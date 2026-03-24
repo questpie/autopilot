cbc2be6c8d92d71175fc3f83904011b68619521d
drepkovsky <dominik.repkovsky@gmail.com>
2026-03-24 11:35:11 +0100

    chore: remove web

--- apps/web/src/routes/docs.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { DocsLayout } from '@/components/DocsLayout'
-
-export const Route = createFileRoute('/docs')({
-	component: DocsLayout,
-})
